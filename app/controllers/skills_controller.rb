588871315df2aa19f572303a90f70e77e29fb060
winifredf <winifred.dykes@hotmail.com>
2020-07-05 18:04:53 -0400

    built show route in skills controller.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,6 +1,11 @@
 class SkillsController < ApplicationController
-    get 'skills' do
+    get '/skills' do
       @skills = Skill.all
       erb :"skills/index"
+    end
+
+    get '/skills/:id' do
+        @skill = Skill.find_by_id(params["id"])
+        erb :"skills/show"
     end
 end
