24a89d05eec5d726352e46b741bdcaf0939be468
winifredf <winifred.dykes@hotmail.com>
2020-07-05 16:48:11 -0400

    migrated users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200705203642) do
+ActiveRecord::Schema.define(version: 20200705204521) do
 
   create_table "skills", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
