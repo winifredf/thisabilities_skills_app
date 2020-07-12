db3639c1c301259f7152227b12d82f9782b7b042
winifredf <winifred.dykes@hotmail.com>
2020-07-11 22:20:30 -0400

    added column password digest (string) to users table and rake db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200705204521) do
+ActiveRecord::Schema.define(version: 20200712021527) do
 
   create_table "skills", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20200705204521) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
