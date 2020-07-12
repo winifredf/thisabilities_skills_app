54c79e4cf2565b3c8f522b6d860e004f9c406760
winifredf <winifred.dykes@hotmail.com>
2020-07-12 14:42:09 -0400

    added post login method to sessions controller, and told to find by user's email.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,10 @@ class SessionsController < ApplicationController
     get '/login' do
         erb :"sessions/login"
     end
+
+    post '/login' do
+        binding.pry
+        user = User.find_by(email: params[:email])
+
+    end
 end
