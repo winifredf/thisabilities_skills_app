145b5317e526bc6e51783ac14373cab73032fcd5
winifredf <winifred.dykes@hotmail.com>
2020-07-13 00:48:45 -0400

    added missing end for helpers method in application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,10 @@ class ApplicationController < Sinatra::Base
   helpers do
     def logged_in?
       session[:user_id]
+    end
   end
 
   def current_user
     @user ||= User.find_by(id: session[:user_id])
-
+  end
 end
