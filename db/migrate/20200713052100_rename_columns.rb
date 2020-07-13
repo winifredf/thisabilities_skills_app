4b42141db991cd015bdb071a0f543f9ad9eaf565
winifredf <winifred.dykes@hotmail.com>
2020-07-13 01:39:02 -0400

    filled in table to rename title to skill and body to skill_description.

--- db/migrate/20200713052100_rename_columns.rb
+++ db/migrate/20200713052100_rename_columns.rb
@@ -1,8 +1,6 @@
 class RenameColumns < ActiveRecord::Migration
   def change
-    rename_column :skills, 
+    rename_column :skills, :title, :body
+    rename_column :skills, :skill, :skill_description
   end
 end
-
-title = skill
-body = skill description
