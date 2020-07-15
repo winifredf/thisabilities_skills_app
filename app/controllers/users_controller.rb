c68fd69dcf64ddf93c1a19933593afc99cc519f7
winifredf <winifred.dykes@hotmail.com>
2020-07-14 21:05:29 -0400

    finished creating my if/else statement for my post users route in signup.erb file. save new user and redirect to /skills, else redirect to users/signup page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,12 @@ class UsersController < ApplicationController
 
     post "/users" do
         @user = User.new(params)
+        if @user && @user.save
+            session[:user_id] = @user.id
+            redirect "/skills"
+        else
+            erb :"users/signup"
+        end
     end
 
 end
